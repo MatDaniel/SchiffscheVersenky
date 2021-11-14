35ba6cd727e56fd7557f8ae64c238d5e471c8693
Lima X <35961248+Lima-X@users.noreply.github.com>
2021-11-14 20:37:45 +0100

    entrypoint

--- client/EntryPoint.cpp
+++ client/EntryPoint.cpp
@@ -1,7 +1,30 @@
+#include "NetworkQueue.h"
 #include "draw/Game.hpp"
 #include "draw/scenes/MenuScene.hpp"
 
-int main(int argc, const char* argv[])
-{
-    return Game::run<MenuScene>();
+int main(
+	int         argc,
+	const char* argv[]
+) {
+	auto ServerAddress = DefaultServerAddress;
+	auto PortNumber = DefaultPortNumber;
+	if (argc >= 2)
+		ServerAddress = argv[1];
+	if (argc >= 3)
+		PortNumber = argv[2];
+
+
+	long Result = 0;
+	ServerIoController NetworkManager(
+		ServerAddress,
+		PortNumber,
+		&Result);
+	SsAssert(Result < 0,
+		"failed to connect client socket to server for io operation with: %d\n",
+		Result);
+
+
+
+	return Game::run<MenuScene>();
+
 }
